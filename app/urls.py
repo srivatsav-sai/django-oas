231c97a2c412555fb435bba34a815449a8c61155
Muskaan Mahindrakar <65728608+Mushu0808@users.noreply.github.com>
2021-05-09 22:34:18 +0530

    Url Patterns

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [ 
+urlpatterns = [  
     path('admin/', admin.site.urls),
     path('', include("login.urls")),
     path('student/', include("student.urls")),
